ba208e7b9f9969d554b073e2648c2a66c5d8c6d2
Aman Mavai <amanmavai@gmail.com>
2023-09-18 11:54:50 +0530

    add root loader

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,18 @@
 import {cssBundleHref} from "@remix-run/css-bundle";
-import type {LinksFunction} from "@remix-run/node";
+import type {LinksFunction, LoaderFunctionArgs} from "@remix-run/node";
+import {json} from "@remix-run/node";
 import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from "@remix-run/react";
 import tailwindStyles from "./styles/tailwind.css";
+import {getUser} from "./session.server";
 
 export const links: LinksFunction = () => [
   {rel: "stylesheet", href: tailwindStyles},
   ...(cssBundleHref ? [{rel: "stylesheet", href: cssBundleHref}] : []),
 ];
+
+export const loader = async ({request}: LoaderFunctionArgs) => {
+  return json({user: await getUser(request)});
+};
 
 export default function App() {
   return (
