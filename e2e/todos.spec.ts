a1c03f7fc620cb24e9e6fd3ee4b02dc5bb1744be
Aman Mavai <amanmavai@gmail.com>
2023-09-23 16:36:50 +0530

    feat: add todos e2e test

---
+++ e2e/todos.spec.ts
@@ -0,0 +1,43 @@
+import {test, expect} from "@playwright/test";
+
+const TODO_ITEMS = ["buy some cheese", "feed the cat", "book a doctors appointment"];
+
+test.describe("Todo App", () => {
+  test.beforeEach(async ({page}) => {
+    await page.goto("http://localhost:3000/todos");
+  });
+
+  test("loads todos", async ({page}) => {
+    // Expects page to have a heading.
+    await expect(page.getByRole("heading", {name: "Todos List"})).toBeVisible();
+  });
+
+  test("adds todos", async ({page}) => {
+    const todoItemTextBox = page.getByRole("textbox", {name: "todo-text"});
+
+    await todoItemTextBox.click();
+    await todoItemTextBox.fill(TODO_ITEMS[0]);
+    await page.getByRole("button", {name: /Add/i}).click();
+
+    await todoItemTextBox.fill(TODO_ITEMS[2]);
+    await todoItemTextBox.click();
+    await page.getByRole("button", {name: /Add/i}).click();
+
+    const todoItem = page.getByTestId("todo-item");
+    await expect(todoItem).toHaveCount(2);
+    await expect(todoItem).toHaveText([TODO_ITEMS[0], TODO_ITEMS[2]]);
+  });
+
+  test("toggles a todo", async ({page}) => {
+    await page.getByRole("button", {name: /Toggle Todo/i}).click();
+
+    const todoClass = await page.getAttribute(".todo", "class");
+    expect(todoClass).toContain("completed");
+  });
+
+  test("deletes a todo", async ({page}) => {
+    await page.getByRole("button", {name: /Delete Todo/i}).click();
+    const todoList = await page.$$(".todo");
+    expect(todoList.length).toBe(0);
+  });
+});
